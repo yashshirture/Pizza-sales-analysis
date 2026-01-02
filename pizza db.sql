0da38ae57f70385a4cb2d5aba2385994eb2446c9
Yash Shirture <yashatwork623@gmail.com>
2026-01-03 00:56:46 +0530

    Add files via upload

---
+++ pizza db.sql
@@ -0,0 +1,37 @@
+SELECT * FROM pizza_sales;
+
+-- Total Revenue
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+-- Avg Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales;
+
+--  Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales;
+
+-- Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales;
+
+-- Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales;
+
+-- Daily Trend for Total Orders
+SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date);
+
+-- Hourly Trend for Orders
+SELECT DATEPART(HOUR, order_time) as order_hours, COUNT(DISTINCT order_id) as total_orders
+from pizza_sales
+group by DATEPART(HOUR, order_time)
+order by DATEPART(HOUR, order_time);
+
+--  % of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category;
+
